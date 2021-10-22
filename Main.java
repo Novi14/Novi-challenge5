ec3f0e952a17b2a5a8f89fd0d214b43373c9e2aa
Novi <santikanovi220@gmail.com>
2021-10-21 22:51:48 -0700

    first commit

---
+++ Main.java
@@ -0,0 +1,42 @@
+
+class Main {
+    public static void main(String[] args) {
+      Heroes hero = new Heroes();
+  
+      hero.setProfile("Novi santika", 20, 150);
+      hero.profile();
+      
+  
+      // jalankan aktivitas disini...
+      hero.jalan();
+      hero.jalan();
+      hero.jalan();
+      hero.jalan();
+
+      hero.Lari();
+
+      hero.Makan();
+      hero.Makan();
+      hero.Makan();
+
+      hero.Minum();
+      hero.Minum();
+      hero.Minum();
+      hero.Minum();
+      hero.Minum();
+
+      hero.Lompat();
+      hero.Lompat();
+      hero.Lompat();
+      hero.Lompat();
+      hero.Lompat();
+
+      hero.Duduk();
+      hero.Duduk();
+      hero.Duduk();    
+    
+      // tampilkan status terkini...
+      hero.profile();
+      
+    }
+  }
